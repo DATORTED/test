96e1395ecc27bee2d7ee1e8362233aa9ca8b25b7
thuphuong55 <thuphuong55203@gmail.com>
2024-11-02 11:21:45 +0700

    tich:

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+return a*b;}
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a,b;
+	cout<<"nhap a va b=";
+	cin>> a,b;
+	cout<<"tich la="<<tich(a,b)<<endl;
 	system("pause");
 	return 0;
 }
