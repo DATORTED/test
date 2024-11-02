c6dad6c40eab48920f035715540884d8d02208cf
DATORTED <dangtandat43@gmail.com>
2024-11-02 11:09:48 +0700

    test tong

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+return a+b;}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a,b;
+	cout<<"nhap a,b:";
+	cin>>a>>b;
+	cout<<"tong a va b ="<<tong(a,b);<<endl;
 	system("pause");
 	return 0;
 }
