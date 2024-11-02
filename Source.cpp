d69096ed8868c165bd1053603dee5898522bd928
TerryWolf1109 <minhnnguyen1109@gmail.com>
2024-11-02 11:29:03 +0700

    thuong

--- Source.cpp
+++ Source.cpp
@@ -6,17 +6,22 @@ int hieu(int a, int b) {
 	return a-b;
 }
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b){
+return a/b;}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 int a,b;
 cin>>a>>b;
+<<<<<<< HEAD
 
 cout<<"hieu a va b=" << hieu(a, b);
 =======
 cout<<hieu(a,b);
 cout<<"aaaaaaaaa";
+=======
+cout<<thuong(a,b);
+>>>>>>> 144f862 (thuong)
 	system("pause");
 	return 0;
 }
