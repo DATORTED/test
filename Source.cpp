1c20a380615faab4aa943f4dcac9d9460394f541
TanDat <dangtandat43@gmail.com>
2024-11-09 10:37:27 +0700

    dat

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+return a+b;}
 int hieu(int a, int b) {
 	return a-b;
 }
@@ -14,6 +15,7 @@ int main()
 int a,b;
 cin>>a>>b;
 cout<<thuong(a,b);
+cou<<tong(a,b);
 	system("pause");
 	return 0;
 }
